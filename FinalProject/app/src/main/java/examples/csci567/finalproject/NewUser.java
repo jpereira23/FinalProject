9db2f8104c20435e47bd83ad1b85cec3df25408b
eroberts20 <eroberts20@mail.csuchico.edu>
2017-03-24 22:57:41 -0700

    Picture Taking Intent and saving file path of image to user in the pictureName variable

--- FinalProject/app/src/main/java/examples/csci567/finalproject/NewUser.java
+++ FinalProject/app/src/main/java/examples/csci567/finalproject/NewUser.java
@@ -1,28 +1,67 @@
 package examples.csci567.finalproject;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+import static android.provider.Telephony.Mms.Part.FILENAME;
+
 public class NewUser extends AppCompatActivity {
     private int count = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private Button takePicture;
+    private ImageView imageView;
+    private String file_path=null;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,27 +77,61 @@ public class NewUser extends AppCompatActivity {
         RealmResults<Entity> results = query.findAll();
         count = results.size();
 
+        imageView = (ImageView) findViewById(R.id.imageview);
+        takePicture = (Button) findViewById(R.id.picture_button);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askForPermission(Manifest.permission.CAMERA, CAMERA);
+                takePicture(view);
+               // Log.e((String)imageView.getTag(),(String) imageView.getTag());
+
+            }
+
+        });
+
         Button createUser = (Button) findViewById(R.id.button);
         createUser.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view)
-            {
+            @Override
+            public void onClick(View view) {
                 EditText text = (EditText) findViewById(R.id.editText);
                 String aText = text.getText().toString();
                 User user = new User();
                 user.setName(text.getText().toString());
                 TextView text1 = (TextView) findViewById(R.id.textView);
-                TextView text2 = (TextView) findViewById(R.id.textView2);
+               // TextView text2 = (TextView) findViewById(R.id.textView2);
                 text1.setText(user.getName());
-                text2.setText(user.getPicture());
-                sendToDatabase(user.getName(), user.getPicture());
+                //text2.setText(user.getPicture());
+                sendToDatabase(user.getName(), file_path);
                 finish();
             }
 
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void sendToDatabase(String name, String pictureName)
-    {
+    private void askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(NewUser.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(NewUser.this, permission)) {
+
+                //This is called if user has denied the permission before
+                //In this case I am just asking the permission again
+                ActivityCompat.requestPermissions(NewUser.this, new String[]{permission}, requestCode);
+
+            } else {
+
+                ActivityCompat.requestPermissions(NewUser.this, new String[]{permission}, requestCode);
+            }
+        }
+    }
+
+
+
+    private void sendToDatabase(String name, String pictureName) {
         Realm realm = Realm.getDefaultInstance();
         realm.beginTransaction();
         count += 1;
@@ -69,16 +142,102 @@ public class NewUser extends AppCompatActivity {
     }
 
 
-
-    private void createNewUser()
-    {
+    private void createNewUser() {
         Button addUser = (Button) findViewById(R.id.button);
-        addUser.setOnClickListener( new View.OnClickListener() {
-            @Override public void onClick(View v) {
+        addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
 
         });
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("NewUser Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+    private Uri file;
+    public void takePicture(View view) {
+        verifyStoragePermissions(this);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        file_path = file.getPath();
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(file);
+            }
+        }
+    }
+    String mCurrentPhotoPath;
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 }
